4b2b6a27563398689fa228c45212e0c313df542a
rahowa <roninfromsvf@gmail.com>
2019-11-23 12:34:20 +0300

    done with stream page

--- app/stream_views.py
+++ app/stream_views.py
@@ -1,12 +1,28 @@
-from flask import Response, render_template
+from flask import Response, render_template, g
 
 from app import app
-from app.camera import CamReader
+from app.camera import (CamReader, Image, 
+                        wrap_image, encode_image)
+from app.drawer import DrawZone
 
+
+def get_video_capture(cam_id: int) -> CamReader:
+    caputure = getattr(g, "_capture", None)
+    if caputure is None:
+        caputure = g._capture = CamReader(cam_id)
+    return caputure
+
+def get_drawer(config_path: str) -> DrawZone:
+    drawer = getattr(g, "_zone_drawer", None)
+    if drawer is None:
+        drawer = g._zone_drawer = DrawZone(config_path)
+    return drawer
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(CamReader(0).gen_encoded_frame(),
+    capture = get_video_capture(0)
+    drawer = get_drawer("/Users/valentinevseenko/Training/workzone/robot_work_zone_estimation/YOUR_CUSTOM_CONFIG.json")
+    return Response(capture.gen_encoded_frame(drawer),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
