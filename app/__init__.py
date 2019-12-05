5ed708b15260f0e5dfb7ac219affd638ff7f7b9d
rahowa <roninfromsvf@gmail.com>
2019-12-05 21:25:12 +0300

    refactor app instantiation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from config import MainConfig
+from .extensions import mongo
 
-app = Flask(__name__)
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    mongo.init_app(app)
+    return app 
+
+app = create_app(MainConfig)
 
 from app import views
 from app import stream_views
