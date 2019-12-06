68336ca098e1adb20b09037f0971e0b78feb9a46
rahowa <roninfromsvf@gmail.com>
2019-12-06 14:15:11 +0300

    make possible to return int values as class for object\faces

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Union
+from collections import deque
 from dataclasses import dataclass 
 
 import cv2
@@ -6,17 +7,19 @@ import werkzeug
 import numpy as np 
 import jsonpickle
 from flask import Flask, request, render_template, Response
+import face_recognition as fr 
 
 from app import app
 from TEST_face_detection import BBox, BBoxes
 from .camera import Image
-from .extensions import mongo
-
+from .extensions import mongo, PyMongo
+from .mongo_controller import MongoController
+from nn_inference.external_wrappers import FaceDetectionLibWrapper
 
 @dataclass
 class DetectionResult:
     image: np.ndarray 
-    labels: List[str]
+    labels: List[Union[int, str]]
     bboxes: BBoxes
 
 
@@ -25,20 +28,22 @@ def PLACEHOLDER_detect_objects(image: Image) -> DetectionResult:
     """
     return DetectionResult(image, "human", ((20, 20, 50, 50)))
 
-def PLACEHOLDER_define_workzone(image: Image) -> Image:
-    """
-    """
-    pass
 
-def PLACEHOLDER_read_main_cam(cam_id: int) -> Image:
+def PLACEHOLDER_recognaize_face(image: Image, db_controller: MongoController) -> DetectionResult:
     """
     """
-    pass 
+    face_locations = fr.face_locations(image)
+    face_encodings = fr.face_encodings(image, face_locations)
+    known_face_encodings = db_controller.all_descriptors()
+    labels = deque(maxlen=len(face_encodings))
 
-def PLACEHOLDER_recognaize_face(image: Image) -> DetectionResult:
-    """
-    """
-    return DetectionResult(image, "Ilon mosk", ((20, 20, 50, 50)))
+    for face_encoding in face_encodings:
+        matches = fr.compare_faces(known_face_encodings, face_encoding)
+        label = -1
+        if True in matches:
+            label = matches.index(True)
+        labels.append(label)
+    return DetectionResult(image, labels, face_locations)
 
 
 # @app.route('/workzone', methods=['GET'])
@@ -62,10 +67,10 @@ def face_recognition_pipeline():
         return render_template("session_history.html")
 
 
-# @app.route('/objects', methods=['GET'])
-# def object_detection_pipeline():
-#     image = PLACEHOLDER_read_main_cam(0)
-#     res = PLACEHOLDER_detect_objects(image)
-#     form = None
-#     return render_template("PLACEHOLDER_objects.html", form=form)
+@app.route('/objects', methods=['GET'])
+def object_detection_pipeline():
+    image = PLACEHOLDER_read_main_cam(0)
+    res = PLACEHOLDER_detect_objects(image)
+    form = None
+    return render_template("PLACEHOLDER_objects.html", form=form)
 
