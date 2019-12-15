ba1c8d4ab5268d60a34aa07ae15f3362fa5d6533
rahowa <roninfromsvf@gmail.com>
2019-12-15 21:21:50 +0300

    refactor image processing and descriptors extraction

--- nn_inference/external_wrappers.py
+++ nn_inference/external_wrappers.py
@@ -1,11 +1,11 @@
 import numpy as np
 import face_recognition as fr
 from typing import Union, Any
-from .base_wrapper import BaseWrapper, Image, Descriptor, Descriptors
+from .base_wrapper import BaseWrapper, Image, Descriptor, Descriptors, BBoxes
 
 
 class FaceDetectionLibWrapper(BaseWrapper):
-    def __init__(self, config: Union[str, Any], model: keras.models.Model):
+    def __init__(self, config: Union[str, Any]):
         super().__init__()
         self.config = config
         if isinstance(config, (str)):
@@ -16,12 +16,23 @@ class FaceDetectionLibWrapper(BaseWrapper):
         self.n_upsample = config['number_of_times_to_upsample']
 
     def preprocess_image(self, image: Image) -> Image:
-        return image / 255.0
+        if image.mean() > 1.0:
+            return image / 255.0
+        else:
+            return image
+
+    def get_locations(self, data: Image) -> BBoxes:
+        face_locations = fr.face_locations(data,
+                                           number_of_times_to_upsample=self.n_upsample,
+                                           model=self.model
+        return face_locations
+
+    def get_encodings(self, data: Image, bboxes: BBoxes) -> Descriptors:
+        face_encodings = fr.face_encodings(data, bboxes)
+        return face_encodings
 
     def predict(self, data: Image) -> Descriptors:
         data = self.preprocess_image(data)
-        face_locations = fr.face_locations(data, 
-                                           number_of_times_to_upsample=self.n_upsample,
-                                           model=self.model)
-        face_encodings = fr.face_encodings(data, face_locations)
+        face_locations = self.get_locations(data)
+        face_encodings = self.get_encodings(data, face_locations)
         return face_encodings
