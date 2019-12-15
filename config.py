98de7986b80d629e2976176255aaab1ce3a10422
rahowa <roninfromsvf@gmail.com>
2019-12-15 21:22:21 +0300

    update passwords

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-import os 
+import os
+from dotenv import load_dotenv
 
+load_dotenv(verbose=True)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class MainConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "BNTU_THE_BEST_1488"
-    MONGO_URI  = os.environ.get("MONGO_URI")
+    MONGO_URI = os.environ.get("MONGO_URI") or "localhost"
