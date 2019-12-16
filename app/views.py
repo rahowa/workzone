886b7448d7bf040f7cfcc6ed5a41f99749012430
rahowa <roninfromsvf@gmail.com>
2019-12-16 19:51:57 +0300

    add view for database updating

--- app/views.py
+++ app/views.py
@@ -1,24 +1,27 @@
 from typing import List, Union
 from collections import deque
-from dataclasses import dataclass 
+from dataclasses import dataclass
 
 import cv2
-import werkzeug
-import numpy as np 
+import numpy as np
 import jsonpickle
-from flask import Flask, request, render_template, Response
-import face_recognition as fr 
+from flask import request, render_template, Response, Blueprint
+import face_recognition as fr
 
-from app import app
-from TEST_face_detection import BBox, BBoxes
 from .camera import Image
-from .extensions import mongo, PyMongo
+from .extensions import mongo
 from .mongo_controller import MongoController
+from .fill_databse import FillDatabase
+
+from nn_inference.base_wrapper import BBoxes
 from nn_inference.external_wrappers import FaceDetectionLibWrapper
+
+
+main = Blueprint('main', __name__)
 
 @dataclass
 class DetectionResult:
-    image: np.ndarray 
+    image: np.ndarray
     labels: List[Union[int, str]]
     bboxes: BBoxes
 
@@ -29,13 +32,23 @@ def PLACEHOLDER_detect_objects(image: Image) -> DetectionResult:
     return DetectionResult(image, "human", ((20, 20, 50, 50)))
 
 
-def PLACEHOLDER_recognaize_face(image: Image, db_controller: MongoController) -> DetectionResult:
+def PLACEHOLDER_recognaize_face(image: Image,
+                                db_controller: MongoController) -> DetectionResult:
     """
     """
     face_locations = fr.face_locations(image)
     face_encodings = fr.face_encodings(image, face_locations)
+
+    print("Getting all descriptors... ".center(79))
     known_face_encodings = db_controller.all_descriptors()
+    print("All descriptors are loaded".center(79))
     labels = deque(maxlen=len(face_encodings))
+
+    print(f"num of known face encodings: \
+        {len(known_face_encodings)}".center(79))
+
+    if len(known_face_encodings) == 0:
+        return DetectionResult(image, [-1], [0, 0, 1, 1])
 
     for face_encoding in face_encodings:
         matches = fr.compare_faces(known_face_encodings, face_encoding)
@@ -46,20 +59,15 @@ def PLACEHOLDER_recognaize_face(image: Image, db_controller: MongoController) ->
     return DetectionResult(image, labels, face_locations)
 
 
-# @app.route('/workzone', methods=['GET'])
-# def working_zone_pipeline():
-#     image = PLACEHOLDER_read_main_cam(0)
-#     image = PLACEHOLDER_define_workzone(image)
-#     form = None
-#     return render_template('PLACEHOLDER_workzone.html', form=form)
-
-
-@app.route('/face', methods=['GET', 'POST'])
+@main.route('/face', methods=['GET', 'POST'])
 def face_recognition_pipeline():
     if request.method == 'POST':
         image = np.fromstring(request.data, dtype=np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        detection_result = PLACEHOLDER_recognaize_face(image)
+        print('Recvie image'.center(79))
+        db_controller = MongoController(mongo)
+        print("Connect to databse".center(79))
+        detection_result = PLACEHOLDER_recognaize_face(image, db_controller)
         response = {'message': f"{detection_result.labels} was detected"}
         response = jsonpickle.encode(response)
         return Response(response, status=200, mimetype="application/json")
@@ -67,10 +75,17 @@ def face_recognition_pipeline():
         return render_template("session_history.html")
 
 
-@app.route('/objects', methods=['GET'])
-def object_detection_pipeline():
-    image = PLACEHOLDER_read_main_cam(0)
-    res = PLACEHOLDER_detect_objects(image)
-    form = None
-    return render_template("PLACEHOLDER_objects.html", form=form)
+@main.route("/fill_db")
+def fill_database():
+    controller = MongoController(mongo)
+    face_det = FaceDetectionLibWrapper("./nn_inference/configs/test_fr_hog_config.json")
+    FillDatabase(controller, face_det)("./face_database")
+    return "Workers updated"
 
+
+# @app.route('/objects', methods=['GET'])
+# def object_detection_pipeline():
+#     image = PLACEHOLDER_read_main_cam(0)
+#     res = PLACEHOLDER_detect_objects(image)
+#     form = None
+#     return render_template("PLACEHOLDER_objects.html", form=form)
