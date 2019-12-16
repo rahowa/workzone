4e97042802a34584988acaf02f5f165e5db0dcd8
rahowa <roninfromsvf@gmail.com>
2019-12-16 19:50:45 +0300

    fix incorrect .env loading

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(verbose=True)
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class MainConfig:
