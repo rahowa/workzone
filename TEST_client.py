33db40b575e8165feac1c650c042e4c58d6250c5
rahowa <roninfromsvf@gmail.com>
2019-12-20 23:31:33 +0300

    update client

--- TEST_client.py
+++ TEST_client.py
@@ -17,11 +17,20 @@ content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
 if __name__ == "__main__":
-    img = cv2.imread('./assets/testface.jpg')
-    _, img_encoded = cv2.imencode('.jpg', img)
+    # img = cv2.imread('./assets/testface.jpg')
+    # _, img_encoded = cv2.imencode('.jpg', img)
+    # while True:
+    #     print("Send test image")
+    #     time.sleep(0.5)
+    #     response = requests.post(test_url, data=img_encoded.tostring(),
+    #                              headers=headers)
+    #     print(json.loads(response.text))
+
+    cap = cv2.VideoCapture(0)
     while True:
-        print("Send test image")
-        time.sleep(0.5)
-        response = requests.post(test_url, data=img_encoded.tostring(),
+        time.sleep(1)
+        _, frame = cap.read()
+        _, image_to_send = cv2.imencode('.jpg', frame)
+        response = requests.post(test_url, data=image_to_send.tostring(),
                                  headers=headers)
         print(json.loads(response.text))
