f09661ee9174e836ac53971280f68170bb84fdb7
rahowa <roninfromsvf@gmail.com>
2020-03-25 22:49:03 +0300

    add new blueprint for streams

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import MainConfig
 from .extensions import mongo
 
-from .views import main
-
+from .views import bp_main
+from .errors import bp_errors
+from .stream_views import bp_streams
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(MainConfig)
     mongo.init_app(app)
-    app.register_blueprint(main)
+    app.register_blueprint(bp_main)
+    app.register_blueprint(bp_errors)
+    app.register_blueprint(bp_streams)
     return app
--- app/stream_views.py
+++ app/stream_views.py
@@ -1,26 +1,12 @@
-from flask import Response, render_template, g
+from flask import Response, render_template, Blueprint
 
-from app import app
-from app.camera import (CamReader, Image,
-                        wrap_image, encode_image)
-from app.drawer import DrawZone
+from app.drawer import get_drawer
+from app.camera import get_video_capture
 
 
-def get_video_capture(cam_id: int) -> CamReader:
-    caputure = getattr(g, "_capture", None)
-    if caputure is None:
-        caputure = g._capture = CamReader(cam_id)
-    return caputure
+bp_streams = Blueprint("blueprint_streams", __name__)
 
-
-def get_drawer(config_path: str) -> DrawZone:
-    drawer = getattr(g, "_zone_drawer", None)
-    if drawer is None:
-        drawer = g._zone_drawer = DrawZone(config_path)
-    return drawer
-
-
-@app.route('/video_feed')
+@bp_streams.route('/video_feed')
 def video_feed():
     capture = get_video_capture(0)
     drawer = get_drawer("/Users/valentinevseenko/Training/workzone/robot_work_zone_estimation/YOUR_CUSTOM_CONFIG.json")
@@ -28,6 +14,6 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/workzone_stream')
+@bp_streams.route('/workzone_stream')
 def stream():
     return render_template('stream.html')
