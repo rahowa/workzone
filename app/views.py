6facd07a417e87be0370ffeaec32ee3a5e20891f
rahowa <roninfromsvf@gmail.com>
2020-04-21 15:31:23 +0300

    update views, add view fir object detection, add docs

--- app/views.py
+++ app/views.py
@@ -1,68 +1,83 @@
-# from collections import deque
-from typing import Any, Tuple, Union
-from dataclasses import dataclass
-
-import cv2
 import jsonpickle
-import numpy as np
-from nptyping import Array
+from flask import request
+from typing import Any, Union
 from flask import Response, Blueprint
-from flask import request, render_template
 
-from app.camera import Image
 from app.extensions import mongo
+from app.camera import deocode_image
 from app.fill_databse import FillDatabase
 from app.mongo_controller import MongoController
-
-from app.base_types import BBoxes
-from nn_inference.external_wrappers import FaceRecognitionLibWrapper
+from app.faces_utils import check_persons, detect_faces
+from app.nn_inference.faces.wrappers.face_recognition_lib_wrapper import FaceRecognitionLibWrapper
 
 
 bp_main = Blueprint('blueprint_main', __name__)
 
 
-@dataclass
-class DetectionResult:
-    image: Array[int]
-    labels: Tuple[Union[int, str], ...]
-    bboxes: BBoxes
-    
-
-def recognize_face(frame: Image, db_controller: MongoController) -> DetectionResult:
-    known_face_encodings = db_controller.all_valid_descriptors()
-    if len(known_face_encodings) == 0:
-        return DetectionResult(frame, (-1, ), ((0, 0, 0, 0), ))
-
-    face_det = FaceRecognitionLibWrapper("./nn_inference/configs/test_fr_hog_config.json")
-    face_locations = face_det.get_locations(frame)
-    face_encodings = face_det.get_encodings(frame, face_locations)
-    labels = list()
-    for face_encoding in face_encodings:
-        label = -1
-        matches = face_det.match(face_encoding, known_face_encodings)
-        if True in matches:
-            label = matches.index(True)
-        labels.append(label)
-    return DetectionResult(frame, tuple(labels), face_locations)
-
-
-@bp_main.route('/face', methods=['GET', 'POST'])
-def face_recognition_pipeline() -> Union[Response, Any]:
-    if request.method == 'POST':
-        image = np.fromstring(request.data, dtype=np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        db_controller = MongoController(mongo)
-        detection_result = recognize_face(image, db_controller)
-        response = {'message': f"{detection_result.labels} was detected"}
+@bp_main.route('/face/<target>', methods=['POST'])
+def face_processing(target: str) -> Union[Response, Any]:
+    """
+    Detect or recognize faces on image
+    Parameters
+    ----------
+        target: str
+            'detect' for face detection. Return response with boxes.
+            'recognize' for face recognition. Return response with recognized ids.
+    Return
+    ------
+        Response with target result
+    """
+    image = deocode_image(request.data)
+    if target == "recognize":
+        db_controller = MongoController(mongo) #TODO: cache results
+        recognition_result = check_persons(image, db_controller)
+        response = {'faces_id': str(recognition_result)}
+        response = jsonpickle.encode(response)
+        return Response(response, status=200, mimetype="application/json")
+    elif target == "detect":
+        face_locations = detect_faces(image)
+        response = {'faces_loc': str(face_locations)}
         response = jsonpickle.encode(response)
         return Response(response, status=200, mimetype="application/json")
     else:
-        return render_template("session_history.html")
+        return Response("empty", status=404)
+
+
+@bp_main.route("/object/<target>")
+def objects_processing(target: str) -> Response:
+    """
+    Apply object detection or segmentation on on image
+    Parameters
+    ----------
+        target: str
+            'segmentation' for semantic segmentation. Return response with RLE masks.
+            'detection' for object detection. Return response with detected boxes and classes.
+    Return
+    ------
+        Response with target result
+    """
+    pass
 
 
 @bp_main.route("/fill_db")
 def fill_database() -> str:
+    """
+    Fill database with descriptors extracted from images
+    """
     controller = MongoController(mongo)
     face_det = FaceRecognitionLibWrapper("./nn_inference/configs/test_fr_hog_config.json")
     FillDatabase(controller, face_det)("./face_database")
     return "Workers updated"
+
+
+@bp_main.route('/classes')
+def get_availabel_classes() -> Response:
+    """
+    Return Json with all available object detection and segmentation classes
+    """
+    with open("classes.txt") as cls_file:
+        classes = cls_file.read().splitlines()
+    response = {"total": len(classes),
+                "classes": classes}
+    response = jsonpickle.encode(response)
+    return Response(response, status=200, mimetype="application/json")
