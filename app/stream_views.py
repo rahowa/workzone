ddaa7d0bb213c519225fd6d2772817be8bc78ad9
rahowa <roninfromsvf@gmail.com>
2020-04-21 15:29:47 +0300

    change strame views for multiple targets

--- app/stream_views.py
+++ app/stream_views.py
@@ -1,19 +1,39 @@
 from flask import Response, render_template, Blueprint
 
-from app.drawer import get_drawer
+from app.drawer import get_workzone_drawer, get_face_detection_drawer
 from app.camera import get_video_capture
 
 
 bp_streams = Blueprint("blueprint_streams", __name__)
 
-@bp_streams.route('/video_feed')
-def video_feed():
+
+@bp_streams.route('/video_feed/<operation>')
+def video_feed(operation: str) -> Response:
+    """
+    Parameters
+    ----------
+        operation, str
+            Define image processing operation.
+            Possible variants:
+            ze  - workzone estimation
+            od  - object detection;
+            seg - segmentation;
+            fd  - face detection;
+            fr  - face recognition;
+    Return
+    ------
+        Response with modified frame from camera
+    """
     capture = get_video_capture(0)
-    drawer = get_drawer("/Users/valentinevseenko/Training/workzone/robot_work_zone_estimation/YOUR_CUSTOM_CONFIG.json")
+    drawer = None
+    if operation == "ze":
+        drawer = get_workzone_drawer("YOUR_CUSTOM_CONFIG.json")
+    elif operation == "fd":
+        drawer = get_face_detection_drawer()
     return Response(capture.gen_encoded_frame(drawer),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@bp_streams.route('/workzone_stream')
-def stream():
-    return render_template('stream.html')
+@bp_streams.route('/workzone_stream/<operation>')
+def stream(operation: str):
+    return render_template('stream.html', operation=operation)
