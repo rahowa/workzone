8dd7e6888f4d5740918d51a0c726fd5aa09b8f2b
rahowa <roninfromsvf@gmail.com>
2020-04-21 15:29:07 +0300

    fix init

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ from .views import bp_main
 from .errors import bp_errors
 from .stream_views import bp_streams
 
-def create_app(config):
+
+def create_app(config=MainConfig):
     app = Flask(__name__)
     app.config.from_object(MainConfig)
     mongo.init_app(app)
