a90d62f395943edcd23e15e64485caf91959e494
rahowa <roninfromsvf@gmail.com>
2020-04-21 19:04:59 +0300

    fix import

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import MainConfig
 from .extensions import mongo
 
 from .views import bp_main
-from .errors import bp_errors
+# from .errors import bp_errors
 from .stream_views import bp_streams
 
 
@@ -12,6 +12,6 @@ def create_app(config=MainConfig):
     app.config.from_object(MainConfig)
     mongo.init_app(app)
     app.register_blueprint(bp_main)
-    app.register_blueprint(bp_errors)
+    # app.register_blueprint(bp_errors)
     app.register_blueprint(bp_streams)
     return app
