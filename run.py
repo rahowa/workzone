49593910dabff4da55d71263e41678580821e472
rahowa <roninfromsvf@gmail.com>
2020-04-21 15:31:49 +0300

    update run

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from app import app
-
-if __name__ == "__main__":
+from app import create_app
+#
+# if __name__ == "__main__":
+test_app = create_app()
+test_app.run(host="0.0.0.0", port=1000)
     # app.debug = True
     # app.run(host="0.0.0.0", port=5000)
-    app.run()
+
