a55646383713eb23e447732bfd7fb54757ea2630
rahowa <roninfromsvf@gmail.com>
2020-04-24 12:37:08 +0300

    add function to map segmentation result to rgb image

--- app/nn_inference/common/utils.py
+++ app/nn_inference/common/utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from typing import List, Any
 from app.base_types import Image, Boxes
 
@@ -31,3 +32,27 @@ def chunks(lst: List[Any], n: int):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
+
+def decode_segmap(image, nc=21):
+    label_colors = np.array([(0, 0, 0),  # 0=background
+               # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+
+    r = np.zeros_like(image).astype(np.uint8)
+    g = np.zeros_like(image).astype(np.uint8)
+    b = np.zeros_like(image).astype(np.uint8)
+    for l in range(0, nc):
+        idx = image == l
+        r[idx] = label_colors[l, 0]
+        g[idx] = label_colors[l, 1]
+        b[idx] = label_colors[l, 2]
+    print(image.shape, r.shape, g.shape, b.shape)
+    rgb = np.stack([r, g, b], axis=-1)
+    return rgb
