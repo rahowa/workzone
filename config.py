1bd40aead31006fd7754140a077391bc9c231053
rahowa <roninfromsvf@gmail.com>
2020-04-24 22:03:14 +0300

    add DB NAME

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ load_dotenv(verbose=True)
 class MainConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "BNTU_THE_BEST_1488"
     MONGO_URI = os.environ.get("MONGO_URI") or "localhost"
+    MONGO_DBNAME = os.environ.get("MONGO_DBNAME") or 'workers'
