f0ee715ef04f7e3c390570edc692f003ea25e128
rahowa <roninfromsvf@gmail.com>
2020-04-24 22:00:45 +0300

    refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,9 @@ from app.stream_views import bp_streams
 def create_app(config=MainConfig):
     app = Flask(__name__)
     app.config.from_object(MainConfig)
+
     mongo.init_app(app)
+    
     app.register_blueprint(bp_main)
     # app.register_blueprint(bp_errors)
     app.register_blueprint(bp_streams)
