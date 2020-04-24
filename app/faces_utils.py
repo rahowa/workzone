ffcedac42ec03ca8b56838f11792123be2f8157e
rahowa <roninfromsvf@gmail.com>
2020-04-24 22:01:06 +0300

    fix signature

--- app/faces_utils.py
+++ app/faces_utils.py
@@ -9,13 +9,14 @@ def check_persons(frame: Image, db_controller: MongoController) -> List[int]:
     if len(known_face_encodings) == 0:
         return list()
 
-    face_det = FaceRecognitionLibWrapper({"model_type": "hog", "number_of_times_to_upsample": 1})
-    face_locations = face_det.get_locations(frame)
-    face_encodings = face_det.get_encodings(frame, face_locations)
+    face_det = FaceRecognitionLibWrapper({"model_type": "cnn", "number_of_times_to_upsample": 0})
+    # face_locations = face_det.get_locations(frame)
+    # face_encodings = face_det.get_encodings(frame, face_locations)
+    face_encodings = face_det.predict(frame).descriptors
     labels = list()
     for face_encoding in face_encodings:
         label = -1
-        matches = face_det.match(face_encoding, known_face_encodings)
+        matches = face_det.match(known_face_encodings, face_encoding)
         if True in matches:
             label = matches.index(True)
         labels.append(label)
