3a387114c7bd3ed12ece2f64bc60e50cc59274ff
rahowa <roninfromsvf@gmail.com>
2020-04-24 22:00:22 +0300

    update client

--- TEST_client.py
+++ TEST_client.py
@@ -3,14 +3,18 @@ import json
 import time
 import requests
 
+from app.nn_inference.common.utils import draw_bboxes
+
 
 """
     Original implementation at 
     https://gist.github.com/kylehounslow/767fb72fde2ebdd010a0bf4242371594
 """
 
-addr = 'http://127.0.0.1:5000'
-test_url = addr + '/face/detect'
+
+operation = "recognize"
+addr = 'http://0.0.0.0:1000'
+test_url = addr + f'/face/{operation}'
 path_to_test_image = "./test_image.jpg"
 
 # prepare headers for http request
@@ -18,21 +22,30 @@ content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
 if __name__ == "__main__":
-    # img = cv2.imread('./assets/testface.jpg')
-    # _, img_encoded = cv2.imencode('.jpg', img)
-    # while True:
-    #     print("Send test image")
-    #     time.sleep(0.5)
-    #     response = requests.post(test_url, data=img_encoded.tostring(),
-    #                              headers=headers)
-    #     print(json.loads(response.text))
-
-    # cap = cv2.VideoCapture(0)
-    img = cv2.imread(path_to_test_image)
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
     while True:
         # time.sleep(1)
-        # _, frame = cap.read()
-        _, image_to_send = cv2.imencode('.jpg', img)
+        _, frame = cap.read()
+        _, image_to_send = cv2.imencode('.jpg', frame)
         response = requests.post(test_url, data=image_to_send.tostring(),
                                  headers=headers)
+        if operation == "detect":
+            boxes = response.json()["faces_loc"]
+            if len(boxes) > 0:
+                print(boxes)
+                frame = draw_bboxes(frame, boxes)
+
+        else:
+            ids = response.json()["faces_id"]
+            print(ids)
+
+        cv2.imshow("test case", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
         print(response.text)
+
+    cap.release()
+    cv2.destroyAllWindows()
