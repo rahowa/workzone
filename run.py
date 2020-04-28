aec467e4543418e0c14d949efbf85dac56afc254
rahowa <roninfromsvf@gmail.com>
2020-04-28 21:52:37 +0300

    enable DEBUG mode

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from app import create_app
-#
-# if __name__ == "__main__":
-test_app = create_app()
-test_app.run(host="0.0.0.0", port=1000)
-    # app.debug = True
-    # app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    test_app = create_app()
+    test_app.debug = True
+    test_app.run(host="0.0.0.0", port=1000)
 
