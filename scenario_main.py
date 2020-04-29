3a81b61c276ba808c29658545643b4daa93330fc
rahowa <roninfromsvf@gmail.com>
2020-04-29 10:55:11 +0300

    add new scenario

--- scenario_main.py
+++ scenario_main.py
@@ -1,4 +1,5 @@
 import sys
+import asyncio
 from frozendict import frozendict
 from argparse import ArgumentParser
 
@@ -6,6 +7,7 @@ from argparse import ArgumentParser
 
 from scenarios.scenario_zone_estimation import main_zone_estimation
 from scenarios.scenario_face_detection import main_face_detection
+from scenarios.scenario_zone_protection import main_zone_protection
 
 
 CONTENT_TYPE = 'image/jpeg'
@@ -14,7 +16,7 @@ OPERATIONS = ["object_detection", "zone_estimation", "zone_protection"]
 OPERATION_TO_IMPL = frozendict(
     object_detection=main_face_detection,
     zone_estimation=main_zone_estimation,
-    # zone_protection=main_zone_protection
+    zone_protection=main_zone_protection
 )
 
 
@@ -30,7 +32,11 @@ def main() -> None:
     parser = build_parser()
     args = parser.parse_args()
     main_url = f"http://{args.address}:{args.port}"
-    OPERATION_TO_IMPL[args.operation](main_url)
+    operation = OPERATION_TO_IMPL[args.operation]
+    if asyncio.iscoroutinefunction(operation):
+        asyncio.run(operation(main_url))
+    else:
+        operation(main_url)
 
 
 if __name__ == "__main__":
