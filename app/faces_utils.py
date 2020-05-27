7cee2738401e1fc77b5516e6a433da87c1fe61f2
rahowa <roninfromsvf@gmail.com>
2020-05-27 18:05:29 +0300

    add caching for database query

--- app/faces_utils.py
+++ app/faces_utils.py
@@ -1,17 +1,23 @@
 from typing import List
-from app.base_types import Image, Box, Boxes
+
+from app.base_types import Descriptors
+from app.extensions import cache
+from app.base_types import Image, Boxes
 from app.mongo_controller import MongoController
 from app.nn_inference.faces.wrappers.face_recognition_lib_wrapper import FaceRecognitionLibWrapper
 
 
+@cache.memoize(3000)
+def get_known_encodings(db_controller) -> Descriptors:
+    return db_controller.all_valid_descriptors()
+
+
 def check_persons(frame: Image, db_controller: MongoController) -> List[int]:
-    known_face_encodings = db_controller.all_valid_descriptors()
+    known_face_encodings = get_known_encodings(db_controller)
     if len(known_face_encodings) == 0:
         return list()
 
     face_det = FaceRecognitionLibWrapper({"model_type": "cnn", "number_of_times_to_upsample": 0})
-    # face_locations = face_det.get_locations(frame)
-    # face_encodings = face_det.get_encodings(frame, face_locations)
     face_encodings = face_det.predict(frame).descriptors
     labels = list()
     for face_encoding in face_encodings:
