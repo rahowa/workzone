628393dc7758a4c00e87d4c79f33a7a62bdd5f2d
rahowa <roninfromsvf@gmail.com>
2020-05-27 18:27:36 +0300

    fix batch processing

--- app/nn_inference/segmentation/wrappers/torchvision_segmentation_wrapper.py
+++ app/nn_inference/segmentation/wrappers/torchvision_segmentation_wrapper.py
@@ -1,7 +1,8 @@
+import cv2
 import torch
 import numpy as np
 import torchvision
-from typing import Optional, Sequence, List
+from typing import Sequence, List
 
 from app.nn_inference.common.base_wrapper import BaseWrapper
 from app.base_types import Image
@@ -31,26 +32,28 @@ class TorchvisionSegmentationWrapper(BaseWrapper):
         mean = [0.485, 0.456, 0.406]
         std = [0.229, 0.224, 0.225]
         preprocessing = torchvision.transforms.Compose([
-            torchvision.transforms.ToPILImage(),    
+            torchvision.transforms.ToPILImage(),
             torchvision.transforms.Resize(256),
             torchvision.transforms.CenterCrop(224),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize(mean, std),
         ])
-        return preprocessing(images)
+        return torch.cat([preprocessing(image) for image in images], dim=0)
 
     def predict(self, images: Sequence[Image]) -> List[SegmentationResult]:
+        original_size = images[0].shape[0:2]
+        original_size = (original_size[1], original_size[0])
         ready_images = self.preprocess(images)
         self.model.eval()
         with torch.no_grad():
-            if len(ready_images.shape) == 3:
+            # if len(ready_images.shape) == 3:
+            #     predictions = self.model(ready_images.unsqueeze(0))['out']
+            #     mask = torch.argmax(predictions.squeeze(), dim=0).cpu().numpy()
+            #     mask = cv2.resize(mask.astype(np.uint8), original_size)
+            #     return [SegmentationResult(mask)]
+            # else:
                 predictions = self.model(ready_images.unsqueeze(0))['out']
-                mask = torch.argmax(predictions.squeeze(), dim=0).cpu().numpy()
-                print("MASK SHAPE ", mask.shape)
-                return [SegmentationResult(mask)]
-            else:   
-                predictions = self.model(ready_images.unsqueeze(0))['out']
-                mask = torch.argmax(predictions, dim=1).cpu().numpy()
-                print("MASK SHAPE ", mask.shape)
-                return [SegmentationResult(mask[idx])
-                        for idx in mask.shape[0]]
+                masks = torch.argmax(predictions, dim=1).cpu().numpy()
+                masks_resized = list(map(lambda img: cv2.resize(img.astype(np.uint8), original_size), masks))
+                return [SegmentationResult(mask)
+                        for mask in masks_resized]
