84a89ca47a83963f90c8ff00d22bafb377d5c787
rahowa <roninfromsvf@gmail.com>
2020-05-27 18:25:59 +0300

    add caching params

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ class MainConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "BNTU_THE_BEST_1488"
     MONGO_URI = os.environ.get("MONGO_URI") or "localhost"
     MONGO_DBNAME = os.environ.get("MONGO_DBNAME") or 'workers'
+    CACHE_TYPE = "simple"
+    CACHE_DEFAULT_TIMEOUT = 300
+    CACHE_MEMCACHED_SERVERS = ("127.0.0.1:11211",)
