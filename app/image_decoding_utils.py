63d97a47da90a65646c3e3dd25f123846f37e62a
rahowa <roninfromsvf@gmail.com>
2020-05-27 18:05:55 +0300

    add convertion of Imgae to str

--- app/image_decoding_utils.py
+++ app/image_decoding_utils.py
@@ -1,11 +1,19 @@
 import cv2
 import numpy as np
+import base64
+
 from app.base_types import Image
 
 
 def encode_image(image: Image) -> bytes:
-    encoded_img = cv2.imencode(image, 'jpeg')[1]
+    encoded_img = cv2.imencode('jpeg', image)[1]
     return encoded_img.tobytes()
+
+
+def image_to_str(image: Image) -> str:
+    encoded_img = cv2.imencode(".jpeg", image)[1]
+    return base64.encodebytes(encoded_img.tobytes()).decode("utf-8")
+
 
 def deocode_image(encoded_image: bytes) -> Image:
     image = np.fromstring(encoded_image, dtype=np.uint8)
