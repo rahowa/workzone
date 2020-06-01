c13e1812fe586f6329f6320625804ff706422aba
rahowa <roninfromsvf@gmail.com>
2020-06-01 23:11:44 +0300

    remove helmet detection from OD wrapper; convert returned values to pickable format

--- app/nn_inference/detection/wrappers/detection_wrapper.py
+++ app/nn_inference/detection/wrappers/detection_wrapper.py
@@ -1,11 +1,6 @@
-import cv2
 import torch
-import numpy as np
 from pathlib import Path
 from typing import Sequence, Iterator, List
-
-from gluoncv import model_zoo, data, utils
-import mxnet as mx
 
 from app.nn_inference.common.base_wrapper import BaseWrapper
 from app.base_types import Image as BaseImage
@@ -15,9 +10,9 @@ from app.nn_inference.detection.yolo_v3.models import *
 from app.nn_inference.detection.yolo_v3.utils.datasets import *
 from app.nn_inference.detection.yolo_v3.utils.utils import *
 
-from app.nn_inference.common.utils import chunks
 
 HELMET_DETECT_EVENT = 2
+
 
 class YOLOWrapper(BaseWrapper):
     def __init__(self, batch_size: int = 2,
@@ -29,22 +24,15 @@ class YOLOWrapper(BaseWrapper):
         self.weights_path = base_path / "weights" / "yolov3.weights"
         self.model_config = base_path / "config" / "yolov3.cfg"
 
-        self.helmetnet_weights_path = current_dir.parent / "helmetnet" / "weights" / "darknet.params"
-
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.ctx = mx.gpu() if torch.cuda.is_available() else mx.cpu()
         self.tensor_type = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
         self.min_score_thresh = min_score_tresh
         self.min_suppression_threshold = min_suppression_thres
         self.img_size = img_size
         self.initial_img_size = None
-
         self.batch_size = batch_size
-
         self.model = Darknet(self.model_config, self.img_size)
-        helmet_network_type = "yolo3_darknet53_voc" # "yolo3_mobilenet1.0_voc", "mobilenet0.25.params"
-        self.helmetnet_model = model_zoo.get_model(helmet_network_type, pretrained=False)
 
     def __repr__(self):
         return f"YOLOv3 model on {self.device}"
@@ -54,17 +42,6 @@ class YOLOWrapper(BaseWrapper):
             """Initialize basic object detection network"""
             self.model.to(self.device)
             self.model.load_darknet_weights(str(self.weights_path))
-
-            """Initialize helmet detection network"""
-            classes = ['hat', 'person']
-            for param in self.helmetnet_model.collect_params().values():
-                if param._data is not None:
-                    continue
-                param.initialize()
-            self.helmetnet_model.reset_class(classes)
-            self.helmetnet_model.collect_params().reset_ctx(self.ctx)
-            self.helmetnet_model.load_parameters(self.helmetnet_weights_path, ctx=self.ctx)
-
             return True
 
         except Exception as e:
@@ -75,8 +52,6 @@ class YOLOWrapper(BaseWrapper):
         self.model.to("cpu")
 
     def preprocess(self, image: Sequence[BaseImage]) -> Iterator[BaseImage]:
-        #print("SHAPE:", images.shape)
-
         input_imgs = transforms.ToTensor()(image)
         input_imgs, _ = pad_to_square(input_imgs, 0)
 
@@ -91,10 +66,11 @@ class YOLOWrapper(BaseWrapper):
         '''Detect objects'''
         with torch.no_grad():
             detections = self.model(preprocessed_image).cpu()
-            detections = non_max_suppression(detections, self.min_score_thresh, self.min_suppression_threshold)
+            detections = non_max_suppression(detections,
+                                             self.min_score_thresh,
+                                             self.min_suppression_threshold)
             if len(detections) == 0:
                 return [DetectionResult()]
-
         detections = rescale_boxes(detections[0], self.img_size, self.initial_img_size)
 
         boxes = list()
@@ -106,23 +82,12 @@ class YOLOWrapper(BaseWrapper):
             if int(cls_pred) != 0:
                 continue
 
-            confs.append(conf)
-            boxes.append((x1, y1, x2, y2))
-            class_ids.append(cls_pred)
+            confs.append(float(conf.numpy()))
+            boxes.append((int(x1.numpy()), int(y1.numpy()),
+                          int(x2.numpy()), int(y2.numpy())))
+            class_ids.append(int(cls_pred.numpy()))
 
         det_res = DetectionResult(boxes=tuple(boxes), class_id=class_ids, conf=confs)
-
-        '''Detect helmets'''
-        x, orig_img = data.transforms.presets.yolo.load_test(preprocessed_image, short=416)
-        x = x.as_in_context(self.ctx)
-        helmetnet_class_ids, helmetnet_scores, helmetnet_bboxes = self.helmetnet_model(x)
-
-        # Check if any helmet was detected
-        if (0 in helmetnet_class_ids):
-            # If helmet was detected, add special class to class_id attribute
-            # for being able to find it later to validate helmet presence on the scene
-            det_res.class_id.append(HELMET_DETECT_EVENT)
-
         return det_res
 
     def predict(self, images: Sequence[BaseImage]) -> List[DetectionResult]:
@@ -139,7 +104,6 @@ class YOLOWrapper(BaseWrapper):
 
             preprocessed_image = self.preprocess(images)
             det_res = self.predict_on_image(preprocessed_image)
-
             if len(det_res.boxes) > 0:
                 detection_results.append(det_res)
             else:
@@ -153,7 +117,4 @@ class YOLOWrapper(BaseWrapper):
                     detection_results.append(det_res)
                 else:
                     detection_results.append(DetectionResult())
-
-
         return detection_results
-
